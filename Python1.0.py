fafd1608043a56b71bde274700e8a33ce9c84810
RohanR1 <39036938+RohanR1@users.noreply.github.com>
2020-05-20 15:24:44 -0400

    Add files via upload

---
+++ Python1.0.py
@@ -0,0 +1,13 @@
+
+yearborn = input("What year were you born in?: ")
+
+age = 2020 - int(yearborn)
+
+print("You are " + str(age) + " years old")
+
+
+age1 = input("How old are you?: ")
+
+yearborn = 2020 - int(age1)
+
+print("You were born in " + str(yearborn) + " !")
